ae94a275548a2829b1f4daa91fb9afa846b57570
oggy <oggy22@gmail.com>
2015-06-06 21:07:48 -0700

    - Changed to Version 1.1 - CrashReportWindow which stores the exception into a file

--- Dictionary/App.xaml.cs
+++ Dictionary/App.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+using System.IO;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Dictionary
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			CrashReporterWindow window = new CrashReporterWindow(e);
+			if (window.ShowDialog().Value)
+			{
+				var assembly = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+				string filename = @"\\OGGY-PC\CrashReports\" + Environment.MachineName + "-" + e.Exception.Source + "-" + assembly.Version.ToString() + ".crp";
+				File.WriteAllText(filename, window.txtMessage.Text);
+			}
+			e.Handled = true;
+		}
+	}
 }
---
+++ Dictionary/CrashReporterWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Dictionary
+{
+	/// <summary>
+	/// Interaction logic for CrashReporterWindow.xaml
+	/// </summary>
+	public partial class CrashReporterWindow : Window
+	{
+		public CrashReporterWindow(DispatcherUnhandledExceptionEventArgs e)
+		{
+			InitializeComponent();
+			Exception exp = e.Exception;
+			this.Title = exp.Source + " Crash Report";
+			lblMessage.Content = exp.Source + " has failed. Please report this error by clicking on Report.";
+			txtMessage.Text += "Message:\n" + exp.Message + "\n\n";
+			txtMessage.Text += "Stack:\n" + exp.TargetSite + "\n\n";
+			txtMessage.Text += "Exception:\n" + exp.ToString() + "\n\n";
+		}
+
+		private void btnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+			this.Close();
+		}
+
+		private void btnReport_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = true;
+			this.Close();
+		}
+	}
+}
